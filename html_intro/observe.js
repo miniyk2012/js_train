f67ac591f3abaebe47c1144bb956f16d6d9d3138
Thomas Young <yk_ecust_2007@163.com>
2017-08-10 00:01:08 +0800

    [Train] js的观察者模式，on的实现
    http://www.cnblogs.com/LuckyWinty/p/5796190.html

---
+++ html_intro/observe.js
@@ -0,0 +1,49 @@
+// http://www.cnblogs.com/LuckyWinty/p/5796190.html
+// JS的观察者模式
+var Event = {
+    // 通过on接口监听事件eventName
+    // 如果事件eventName被触发，则执行callback回调函数
+    on: function (eventName, callback) {
+        //我的代码
+        if(!this.handles){
+             this.handles={};    
+        }      
+       if(!this.handles[eventName]){
+            this.handles[eventName]=[];
+       }
+       this.handles[eventName].push(callback);
+    },
+    // 触发事件 eventName
+    emit: function (eventName) {
+        //你的代码
+       if(this.handles[arguments[0]]){
+           for(var i=0;i<this.handles[arguments[0]].length;i++){
+               this.handles[arguments[0]][i](arguments[1]);
+           }
+       }
+    }
+};
+
+// 测试1
+Event.on('test', function (result) {
+    console.log(result);
+});
+Event.on('test', function () {
+    console.log('test');
+});
+Event.emit('test', 'hello world'); // 输出 'hello world' 和 'test'
+// 测试2
+var person1 = {};
+var person2 = {};
+Object.assign(person1, Event);
+Object.assign(person2, Event);
+person1.on('call1', function () {
+    console.log('person1');
+});
+person2.on('call2', function () {
+    console.log('person2');
+});
+person1.emit('call1'); // 输出 'person1'
+person1.emit('call2'); // 没有输出
+person2.emit('call1'); // 没有输出
+person2.emit('call2'); // 输出 'person2'
